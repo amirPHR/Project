cf42ede0064aa6fee06a7f78df28b33e00bb0a8e
Alpha b1 <mmadgholampour@gmail.com>
2024-08-19 22:19:32 +0330

    Add Powersystem.py to git

---
+++ Shutdownsystem.py
@@ -0,0 +1,27 @@
+import os 
+import tkinter as tk 
+
+app = tk.Tk()
+app.geometry('500x300')
+app.title('Shutdown system')
+
+#Label 
+label = tk.Label(text = 'Do you want Shutdown your system? ')
+label.pack()
+
+#Function 
+def system_yes():
+    label.configure(os.system('shutdown /s /t 0'))
+
+def system_no():
+    print('Your system not shut down')
+
+#Button 
+b1 = tk.Button(text = 'Yes' , width = 15 , command = system_yes)
+b1.pack()
+
+b2 = tk.Button(text = 'No' , width = 15 , command = system_no)
+b2.pack()
+
+
+app.mainloop()
